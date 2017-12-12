3b5926407d734f978242ddd67a67276c7b19f215
10154507107@ecnu.cn <10154507107@ecnu.cn>
2017-12-12 17:56:36 +0800

    server尝试

---
+++ bin/server.dart
@@ -0,0 +1,26 @@
+import 'package:redstone/redstone.dart' as app;
+import 'package:shelf/shelf.dart' as shelf;
+import 'package:shelf/shelf_io.dart' as shelf;
+import 'package:shelf_cors/shelf_cors.dart' as shelf_cors;
+
+@app.Route("/head/e/")
+helloWorld() => "Hello, World!  Hello dart !";
+@app.Route("/register/")
+register() => "you are a new menmber";
+@app.Route("/data/")
+getdata() {
+  return getDataFromDB();
+}
+
+getDataFromDB(){
+  //to get data from DB.
+  return '''["name1","name2","name3","name4"]''';
+}
+
+main() {
+  shelf.Middleware middleware = shelf_cors.createCorsHeadersMiddleware();
+  app.setupConsoleLog();
+  app.addShelfMiddleware(middleware);
+  app.start(port: 90);
+}
+
